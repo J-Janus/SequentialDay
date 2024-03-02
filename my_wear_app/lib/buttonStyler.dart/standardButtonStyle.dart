e1e0c9c9fa5a6cece8d6d9de8e2cfe39b881d39b
J-Janus <kuba.janus11@gmail.com>
2024-03-02 19:58:46 +0100

    add_ButtonStyle and dependencies

---
+++ my_wear_app/lib/buttonStyler.dart/standardButtonStyle.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ButtonStyle myButtonStyle = ElevatedButton.styleFrom(
+                        minimumSize: Size(60, 60), padding: EdgeInsets.all(8));
+
+ButtonStyle myButtonStyleGreen = ElevatedButton.styleFrom(
+                        minimumSize: Size(60, 60), padding: EdgeInsets.all(8), backgroundColor: Color.fromARGB(125, 40, 221, 43));
