eda6ad1190a5d51a1df4f57a0a0a303d31d89e9f
J-Janus <kuba.janus11@gmail.com>
2024-03-08 17:34:19 +0100

    timer cubit

---
+++ my_wear_app/lib/watch/cubit/timer_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
+
+class TimerCubit extends Cubit<String> {
+  TimerCubit() : super("00:00:00.00");
+
+  final StopWatchTimer _stopWatchTimer = StopWatchTimer();
+  final _isHours = true;
+  DateTime _timeNow = DateTime.now();
+
+  void start() {
+    _stopWatchTimer.onStartTimer();
+    _timeNow = DateTime.now();
+    final value = _stopWatchTimer.rawTime.listen((event) {
+      final displayTime = StopWatchTimer.getDisplayTime(event, hours: _isHours);
+      emit(displayTime);
+    });
+  }
+
+  void stop() {
+    _stopWatchTimer.onStopTimer();
+  }
+
+  void restart() {
+    _stopWatchTimer.onResetTimer();
+  }
+}
